8f18a1bbaeaf30958774a2c834b2bc30ace81b89
Diego Salgado <dasalgadob@unal.edu.co>
2017-10-25 23:14:03 -0500

    Finished setup of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
-
+  get 'static_pages/about'
   resources :clases do
     resources :grupos, shallow: true
   end
 
-  root 'application#hello'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

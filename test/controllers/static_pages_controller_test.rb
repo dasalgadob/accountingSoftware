f4913ae21bba6fe50b28dc869aeaf0d0bf676cc8
Diego Salgado <dasalgadob@unal.edu.co>
2017-10-26 15:21:29 -0500

    First adding style finished and basic routes defined

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,22 +12,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
     assert_select "title", "Home | "+ @base_title
 
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | "+ @base_title
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success #200 status code
     assert_select "title", "About | "+ @base_title   #Assert content of element in the first argument
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact | "+ @base_title
   end
 
 end

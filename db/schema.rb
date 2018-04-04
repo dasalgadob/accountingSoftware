f3015638f13ef892c8c335c41366c8c1684249d1
Diego Salgado <dasalgadob@unal.edu.co>
2018-04-03 21:45:00 -0500

    Remember attribute created in the model and encrypted for secure use

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319194829) do
+ActiveRecord::Schema.define(version: 20180404022351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(version: 20180319194829) do
     t.integer  "person_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
     t.index ["person_id"], name: "index_users_on_person_id", using: :btree
   end
 

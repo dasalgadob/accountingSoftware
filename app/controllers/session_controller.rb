45b634f838baddfae1f23495cc01a08b03ad78f2
Diego Salgado <dasalgadob@unal.edu.co>
2018-04-05 14:55:15 -0500

    Se cambia pagina despues de inicio de sesion

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in(user)
-      redirect_to user
+      redirect_to "/menu"
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'

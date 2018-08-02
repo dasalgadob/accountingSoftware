d58b1d0463f0581282c89283bcb9f2c328dde569
Diego Salgado <dasalgadob@una.edu.co>
2018-08-02 00:12:44 -0500

    Closed #5 pagination design fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,4 +33,22 @@ function activateSaveButton(edit) {
 function addClassByClick(button){
     $('.ui-widget-content').addClass("dropdown-menu")
 }
+
+
+$(document).ready(function(){
+    console.log("test");
+    $('.pagination .previous_page').text('«');
+    $('.pagination .next_page').text('»');
+
+    $('.pagination').each(function () {
+        var $bar = $(this);
+        $bar.find('a, .current, span').wrapAll('<ul class="pagination">');
+        $bar.find('a, .current, span').wrap('<li>');
+    
+        $bar.find('em').each(function () {
+          $(this).parent().addClass('disabled');
+          $(this).replaceWith('<a href="#">' + $(this).text() + '</a>');
+        });
+    });
+});
 //= require_tree .
